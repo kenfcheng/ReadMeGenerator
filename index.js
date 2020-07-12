620766884c2b23d1de6d5903ef1507c5c5b03567
Ken Cheng <kenfcheng@gmail.com>
2020-07-12 10:51:49 -0400

    Async function

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const axios = require("axios");
 const fs = require('fs');
 const path = require('path');
 async function main(){
-    
+    console.log(`starting`);
+    const userResponse = await inquirer
+    .prompt([
 
