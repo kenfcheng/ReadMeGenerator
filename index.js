7d020468f35912ebf93cd39fe389e10fce17eca8
Ken Cheng <kenfcheng@gmail.com>
2020-07-12 10:55:09 -0400

    prompt inputs added

--- index.js
+++ index.js
@@ -6,4 +6,54 @@ async function main(){
     console.log(`starting`);
     const userResponse = await inquirer
     .prompt([
-
+        {
+            type: "input",
+            message: "What is your GitHub user name?",
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "What is your Project Tittle?",
+            name: "projectTittle"
+        },
+        {
+            type: "input",
+            message: "Provide detail description",
+            name: "projectDescription"
+        },
+        {
+            type: "input",
+            message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+            name: "installationProcess"
+        },
+        {
+            type: "input",
+            message: "Provide instructions for use.",
+            name: "instruction"
+        },
+        {
+            type: "input",
+            message: "Provide instructions examples for use.",
+            name: "instructionExample"
+        },
+        {
+            type: "input",
+            message: "provide License name ",
+            name: "licenseName"
+        },
+        {
+            type: "input",
+            message: "provide License url ",
+            name: "licenseUrl"
+        },
+        {
+            type: "input",
+            message: "please enter git hub user names of the contributor if any (If there are mulitple contributor, seperate names with comma and no space! )",
+            name: "contributorsGitUserName"
+        },
+        {
+            type: "input",
+            message: "Provide examples on how to run tests.",
+            name: "tests"
+        }
+        ]);
